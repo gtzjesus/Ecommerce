8bbd740d12dd9ea86b31a8998a7ad40dfd1b6d2a
gtz.jesus <gtz.jesus@outlook.com>
2023-09-21 14:19:49 -0600

    Setting up react query + react query devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { MenuProvider } from './context/MenuContext';
 import Landing from './ui/app/Landing';
+import GlobalStyles from './styles/GlobalStyles';
 import Menu from './pages/menu/Menu';
 import PageNotFound from './pages/notfound/PageNotFound';
-import GlobalStyles from './styles/GlobalStyles';
-import { MenuProvider } from './context/MenuContext';
+
+// REACT QUERY INTEGRATION
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
       <MenuProvider>
-        <GlobalStyles />
-        <Routes>
-          <Route element={<Landing />}>
-            <Route index element={<Navigate replace to="/" />} />
-          </Route>
-          <Route path="menu" element={<Menu />}>
-            <Route index element={<Navigate replace to="/menu" />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Landing />}>
+              <Route index element={<Navigate replace to="/" />} />
+            </Route>
+            <Route path="menu" element={<Menu />}>
+              <Route index element={<Navigate replace to="/menu" />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
       </MenuProvider>
-    </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 export default App;
