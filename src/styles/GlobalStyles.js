8693f6f185978ea438f8eb6700fd15d3b5cd2a25
gtz.jesus <gtz.jesus@outlook.com>
2023-10-03 17:30:27 -0600

    Main background for app

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -7,7 +7,7 @@ const GlobalStyles = createGlobalStyle`
     --width-description: 80%;
     --width-full-window: 100%;
     --width-half-window: 45vw;
-    --height-full-window: 90vh;
+    --height-full-window: 80vh;
     --height-filled-window: 100vh;
 
 
