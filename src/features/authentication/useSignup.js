22e54910777b43cc3141f30cf8ba49140bc94a52
gtz.jesus <gtz.jesus@outlook.com>
2023-10-12 14:03:05 -0600

    Bug in account creation , + navigation

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,12 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
 import { signup as signupAPI } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router';
 
 export function useSignup() {
+  // USER LEAVE APPLICATION (redirect to home)
+  const navigate = useNavigate();
   const { mutate: signup, isLoading } = useMutation({
     mutationFn: signupAPI,
     onSuccess: (user) => {
-      toast.success('Account created');
+      toast.success('Account created, please verify email!');
+      navigate('/', { replace: true });
+
+      // AUTOMATICALLY LOGIN ACCOUNT
     },
   });
 
