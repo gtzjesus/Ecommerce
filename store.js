441f7191aa0fc03a0a417fa676c3b13c3285ce71
gtz.jesus <gtz.jesus@outlook.com>
2023-11-08 11:43:48 -0700

    Refactoring slices

--- store.js
+++ store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bagReducer from './src/features/bag/bagSlice';
+import favesReducer from './src/features/faves/favesSlice';
 
 const store = configureStore({
   reducer: {
     bag: bagReducer,
+    faves: favesReducer,
   },
 });
 
